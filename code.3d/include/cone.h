57b05d5b161696a1ac218ed699124e062b022350
Quentin Coumes <coumes.quentin@gmail.com>
2019-03-25 14:26:35 +0100

    Code cleanup

--- code.3d/include/cone.h
+++ code.3d/include/cone.h
@@ -3,6 +3,12 @@
 
 #include "object.h"
 
+/**
+ * Build a sphere with an uniform distribution of vertices.
+ *
+ * @param n Number of meridian.
+ * @param p Number of parallels.
+ */
 Object* buildRegularCone(int n, int p);
 
 void drawCone(Object *obj, int c);
