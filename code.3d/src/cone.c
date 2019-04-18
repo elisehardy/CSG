234f4c20c4826e0ff910b511e95f1c00863ee8b8
HARDY ELISE <ehardy@localhost.localdomain>
2019-04-18 09:51:55 +0200

    insidecone

--- code.3d/src/cone.c
+++ code.3d/src/cone.c
@@ -2,60 +2,6 @@
 #include "../include/cone.h"
 
 
-Cone *buildRegularCone(int n, int p) {
-    Cone *cone = (Object *) malloc(sizeof(Object));
-    if (cone == NULL) {
-        errno = ENOMEM;
-        perror("Error - buildRegularCone ");
-        exit(1);
-    }
-    
-    double idouble, jdouble, coneN, coneP;
-    int i, j;
-    
-    cone->n = n;
-    cone->p = p;
-    cone->size = n * p;
-    cone->shape = SHP_CONE;
-    cone->vertex = (G3Xpoint *) calloc(cone->size, sizeof(G3Xpoint));
-    cone->normal = (G3Xvector *) calloc(2 + cone->n, sizeof(G3Xvector));
-    if (!(cone->vertex && cone->normal)) {
-        errno = ENOMEM;
-        perror("Error - buildRegularCone ");
-        exit(1);
-    }
-    
-    coneN = (double) cone->n;
-    coneP = (double) cone->p;
-    
-    cone->vertex[0][X] = 0.;
-    cone->vertex[0][Y] = 0.;
-    cone->vertex[0][Z] = 1.;
-    
-    cone->vertex[1][X] = 0.;
-    cone->vertex[1][Y] = 0.;
-    cone->vertex[1][Z] = 0.;
-    
-    cone->normal[0][X] = 0.;
-    cone->normal[0][Y] = 0.;
-    cone->normal[0][Z] = 1.;
-    
-    cone->normal[1][X] = 0.;
-    cone->normal[1][Y] = 0.;
-    cone->normal[1][Z] = -1.;
-    
-    for (i = 2; i < cone->p; i++) {
-        idouble = (double) i;
-        for (j = 0; j < cone->n; j++) {
-            jdouble = (double) j;
-            cone->vertex[i * (cone->n) + j][0] = cos(PI * 2 * jdouble / coneN) * ((idouble + 1.) / coneP);
-            cone->vertex[i * (cone->n) + j][1] = sin(PI * 2 * jdouble / coneN) * ((idouble + 1.) / coneP);
-            cone->vertex[i * (cone->n) + j][2] = -1;
-        }
-    }
-    
-    return cone;
-}
 
 
 Cone *buildRandomCone(int n, int p) {
@@ -117,30 +63,7 @@ Cone *buildRandomCone(int n, int p) {
 }
 
 
-void drawRegularCone(Cone *cone, int c) {
-    if (cone == NULL) {
-        errno = EFAULT;
-        perror("Error - drawCone ");
-        exit(1);
+bool insideCone(G3Xpoint p){
+    return (p[2]>-1 && p[2]<0) && (p[0]*p[0]+p[1]*p[1]) < (p[2]*p[2]);
     }
-    
-    G3Xpoint *v = cone->vertex;
-    int i, j;
-    
-    glPointSize(1);
-    glBegin(GL_POINTS);
-    
-    glVertex3dv(cone->vertex[0]);
-    glVertex3dv(cone->vertex[1]);
-    
-    glNormal3dv(cone->normal[0]);
-    glNormal3dv(cone->normal[1]);
-    
-    for (i = 0; i < cone->p; i += c) {
-        for (j = 0; j < cone->n; j += c) {
-            glVertex3dv(v[i * (cone->n) + j]);
-        }
-    }
-    
-    glEnd();
-}
+
